554344f030ea1c5b6a0d4d97f61f6d53ee31a735
PranavPatil <pranavpatil69216@gmail.com>
2026-01-16 23:33:32 +0530

    Added validaton for user

--- Server/src/routes/auth.routes.js
+++ Server/src/routes/auth.routes.js
@@ -1,8 +1,11 @@
 import express from "express";
 import {resgisterUser} from "../controllers/auth.controllers.js"
+import { userRegisterValidator } from "../validators/index.js";
+import { validate } from "../middlewares/validator.middleware.js";
 
 const router = express.Router();
 
-router.post("/register",resgisterUser)
+router.post("/register",userRegisterValidator(),validate,resgisterUser)
+// sabse pehle userRegisteredValidator run hota hai uske baad validate mai saare errors jaate hai and then uske baad sab kaam hota hai
 
 export default router;
---
+++ Server/src/validators/index.js
@@ -0,0 +1,24 @@
+import { body } from "express-validator";
+
+const userRegisterValidator = ()=>{
+    return[
+        body("email")
+        .trim()
+        .notEmpty()
+        .withMessage("Email is required.")
+        .isEmail()
+        .withMessage("Please enter a valid email ID."),
+        body("username")
+        .trim()
+        .notEmpty()
+        .withMessage("please fill the username"),
+        body("password")
+        .trim()
+        .notEmpty()
+        .withMessage("please fill your password")
+        .isLength({min:8})
+        .withMessage("password must be of minimum 8 characters"),
+    ]
+}
+
+export {userRegisterValidator}
