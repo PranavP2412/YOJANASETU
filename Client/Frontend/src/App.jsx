15f249c8727c853017e615a2e917a27144734387
Krish Ahirkar <krishahirkar@gmail.com>
2026-01-22 21:46:30 +0530

    app.jsx fixed

--- Client/Frontend/src/App.jsx
+++ Client/Frontend/src/App.jsx
@@ -4,8 +4,8 @@ import LandingPage from './components/LandingPage/Landingpage';
 import SchemeDiscovery from './components/AllScheme/Schemediscovery';
 import LoginPage from './components/Login/LoginPage';
 import VerifyEmailToken from './components/VerifyEmailToken/VerifyEmailToken'; // Make sure to import this
-import ForgotPasswordPage from './components/forgotpassword/ForgotPasswordPage';
-import ResetPasswordPage from './components/resetpassword/ResetPasswordPage';
+import ForgotPassword from './components/forgotpassword/ForgotPasswordPage';
+import ResetPassword from './components/resetpassword/ResetPasswordPage';
 import ProfilePage from './components/Profile/profile';
 import RegisterPage from './components/Register/RegisterPage';
 
@@ -30,8 +30,7 @@ function App() {
             <Route path="/" element={<LandingPage />} />
             <Route path="/schemes" element={<SchemeDiscovery />} />
             <Route path="/login" element={<LoginPage />} />
-            <Route path="/forgotpassword" element={<ForgotPasswordPage />} />
-            <Route path="/Resetpassword" element={<ResetPasswordPage />} />
+            
             <Route path="/Profile" element={<ProfilePage />} />
             
             <Route path="/register" element={<RegisterPage />} />
